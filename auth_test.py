7e13996c7439ae4f3f22c0ca119b4cf575eba584
Mike Morganti <pythomike@gmail.com>
2019-03-10 14:07:31 -0700

    Added readme, first working version (see readme)

--- auth_test.py
+++ auth_test.py
@@ -7,24 +7,38 @@ api = twitter.Api(consumer_key=gear['api_key'],
                   access_token_key=gear['access_token'],
                   access_token_secret=gear['access_token_secret'])
 
+# Final DataFrame
+cols = ['text', 'user', 'created_at', 'id']
+df  = pd.DataFrame(columns=cols)
+
+# First Run - Newest
 fav = api.GetFavorites(screen_name='mjmorganti', count=200, return_json=True)
 # fav = api.GetFavorites(screen_name='mjmorganti', count=200, return_json=True, max_id=1070350854539173888)
 # fav = api.GetFavorites(screen_name='mjmorganti', count=2, return_json=True)
 
-cols = ['text', 'user', 'created_at', 'id']
-df  = pd.DataFrame(columns=cols)
+def favourite_parser():
+  for i in fav:
+    # fave = {
+    #   'text':[i['text']],
+    #   'user':[i['user']['name']],
+    #   'created_at':[i['created_at']],
+    #   'id': [i['id']]    
+    #   # Need a function to parse out all hashtags and dump into a list.
+    #   # 'entities':i['entities']
+    # }
+    # test = pd.DataFrame.from_dict(fave, orient='columns')
+    # df = pd.concat([df, test], axis=0).reset_index(drop=True)
 
-for i in fav:
-  fave = {
-    'text':[i['text']],
-    'user':[i['user']['name']],
-    'created_at':[i['created_at']],
-    'id': [i['id']]    
-    # Need a function to parse out all hashtags and dump into a list.
-    # 'entities':i['entities']
-  }
-  test = pd.DataFrame.from_dict(fave, orient='columns')
-  df = pd.concat([df, test], axis=0).reset_index(drop=True)
+    test= pd.DataFrame.from_dict({
+      'text':[i['text']],
+      'user':[i['user']['name']],
+      'created_at':[i['created_at']],
+      'id': [i['id']]    
+      # Need a function to parse out all hashtags and dump into a list.
+      # 'entities':i['entities']
+    }, orient='columns')
+    df = pd.concat([df, test], axis=0).reset_index(drop=True)
+
   df.to_csv('faves.csv')
 
 
