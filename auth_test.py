082e891fadfbb6ffcd328321c8e5701a7c7e9dc7
Mike Morganti <pythomike@gmail.com>
2019-03-10 15:17:32 -0700

    Looping works, script should exit when < 2 records returned, blocked due to rate limits

--- auth_test.py
+++ auth_test.py
@@ -5,40 +5,48 @@ from env import gear
 api = twitter.Api(consumer_key=gear['api_key'],
                   consumer_secret=gear['api_secret'],
                   access_token_key=gear['access_token'],
-                  access_token_secret=gear['access_token_secret'])
+                  access_token_secret=gear['access_token_secret']
+                  )
 
 # Final DataFrame
 cols = ['text', 'user', 'created_at', 'id']
 df  = pd.DataFrame(columns=cols)
+tweet_id = 0
+batch_length = 2
+running = True
 
 # First Run - Newest
 fav = api.GetFavorites(screen_name='mjmorganti', count=200, return_json=True)
-# fav = api.GetFavorites(screen_name='mjmorganti', count=200, return_json=True, max_id=1070350854539173888)
-# fav = api.GetFavorites(screen_name='mjmorganti', count=2, return_json=True)
 
-def favourite_parser():
+def favourite_parser(fav):
+  global df, tweet_id, batch_length
   for i in fav:
-    # fave = {
-    #   'text':[i['text']],
-    #   'user':[i['user']['name']],
-    #   'created_at':[i['created_at']],
-    #   'id': [i['id']]    
-    #   # Need a function to parse out all hashtags and dump into a list.
-    #   # 'entities':i['entities']
-    # }
-    # test = pd.DataFrame.from_dict(fave, orient='columns')
-    # df = pd.concat([df, test], axis=0).reset_index(drop=True)
-
     test= pd.DataFrame.from_dict({
-      'text':[i['text']],
-      'user':[i['user']['name']],
-      'created_at':[i['created_at']],
-      'id': [i['id']]    
-      # Need a function to parse out all hashtags and dump into a list.
-      # 'entities':i['entities']
-    }, orient='columns')
+                                'text':[i['text']],
+                                'user':[i['user']['name']],
+                                'created_at':[i['created_at']],
+                                'id': [i['id']]    
+                                # Need a function to parse out all hashtags and dump into a list.
+                                # 'entities':i['entities']
+                              }, orient='columns')
     df = pd.concat([df, test], axis=0).reset_index(drop=True)
+  batch_length = len(fav)
+  print("Batch Length: ", batch_length)
+  tweet_id = df.iloc[-1]['id']
+  print("Tweet ID: ", tweet_id)
 
-  df.to_csv('faves.csv')
+def favourite_iterator(maxId):
+  while batch_length > 1:
+    fav = api.GetFavorites(screen_name='mjmorganti', count=200, return_json=True, max_id=maxId)
+    favourite_parser(fav)
+    maxId = tweet_id 
+
+
+favourite_parser(fav)
+favourite_iterator(tweet_id)
+
+print(df.info())
+
+df.to_csv('faves.csv')
 
 
